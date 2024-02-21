47c5f7c866a4af4e183b6dd17f6c9efa8b481a9a
dannyfrelink <danny-frelink@hotmail.com>
2024-02-21 13:41:59 +0100

    update: disable NextJS default loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  devIndicators: {
+    buildActivity: false,
+  },
 };
 
 const withTM = require("next-transpile-modules")(["tailwindcss"]);
