4d3376cea4e198c87353f4faa05d615425959d85
dannyfrelink <danny-frelink@hotmail.com>
2024-02-22 10:50:43 +0100

    update: tring to add useState  to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import { AppProvider } from "@/config/AppContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [inBrowser, setInBrowser] = useState(false);
+  useEffect(() => setInBrowser(true), []);
+
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <>
+      {inBrowser ? (
+        // running in browser load Auth
+        <AppProvider>
+          <Component {...pageProps} />
+        </AppProvider>
+      ) : (
+        // building or running in pipeline
+        <Component {...pageProps} />
+      )}
+    </>
   );
 }
