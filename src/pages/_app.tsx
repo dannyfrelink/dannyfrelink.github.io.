0cb2c26d2029ae5f6cfbc56a3c328750e5c95b78
dannyfrelink <danny-frelink@hotmail.com>
2024-02-22 10:46:51 +0100

    remove: HelmetProvider in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import { AppProvider } from "@/config/AppContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { HelmetProvider } from "react-helmet-async";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <HelmetProvider>
-      <AppProvider>
-        <Component {...pageProps} />
-      </AppProvider>
-    </HelmetProvider>
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
   );
 }
