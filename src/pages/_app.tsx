f69550398d27397048da56c477449366bcb1d1ed
dannyfrelink <danny-frelink@hotmail.com>
2024-02-22 13:04:42 +0100

    update: move og:type value to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, router, pageProps }: AppProps) {
           <meta property="og:title" content={metaData.title} />
           <meta property="og:description" content={metaData.description} />
           <meta property="og:url" content={metaData.url} />
-
+          <meta property="og:type" content="website" />
           <meta
             property="og:image"
             content={`https://www.reisfeeld.nl${metaData.image}`}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,7 +19,6 @@ const Document = () => {
           rel="stylesheet"
         />
 
-        <meta property="og:type" content="website" />
         <meta property="og:image:width" content="2500" />
         <meta property="og:image:height" content="1667" />
         <meta property="og:image:type" content="image/jpeg" />
